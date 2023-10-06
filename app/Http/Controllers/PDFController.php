92e3902e0ae3337ba73879e0fce989dd1fd37560
Miguel Collantes <miguel@make.pe>
2023-10-05 23:06:38 -0500

    Hotfix

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -41,7 +41,8 @@ class PDFController extends Controller
         $fullname = $request->file('archivos')->getClientOriginalName();
         $name = explode('.', $fullname)[0];
         $extension = $request->file('archivos')->getClientOriginalExtension();
-        $new_name = filter_var(($name."-".time()), FILTER_SANITIZE_STRING).".".$extension;
+        //$new_name = filter_var(($name."-".time()), FILTER_SANITIZE_STRING).".".$extension;
+        $new_name = "reporte-importado-".time().".".$extension;
         $filename = $request->file('archivos')->storeAs('originales', $new_name);
         session(['info_global' => []]);
         
