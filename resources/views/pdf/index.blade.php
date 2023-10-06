ba897400925401bdf0fc18a9772b3e1bf1efbb63
Miguel Collantes <miguel@make.pe>
2023-10-05 23:09:11 -0500

    Hotfix

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -34,10 +34,10 @@
                             <div class="fw-semibold">{{$document->created_at}}</div>
                           </td>
                           <td>
-                            <div class="fw-semibold"><a href="{{$storage_url."/reportes/".$document->converted_file}}">{{$document->converted_file}}</a></div>
+                            <div class="fw-semibold"><a href="{{$storage_url."/reportes/".$document->converted_file}}" target="_blank">{{$document->converted_file}}</a></div>
                           </td>
                           <td>
-                            <div class="fw-semibold"><a href="{{$storage_url."/originales/".$document->original_file}}">{{$document->original_file}}</a></div>
+                            <div class="fw-semibold"><a href="{{$storage_url."/originales/".$document->original_file}}" target="_blank">{{$document->original_file}}</a></div>
                           </td>
                         </tr>
                         @endforeach
